353b248ee647b84cc5c9e35633e9e58b687a9c59
knevctt <knevctt@outlook.com>
2024-08-20 19:28:28 -0300

    Initial commit

---
+++ GestaoLojaRoupa/src/main/java/app/GestaoLojaRoupa/GestaoLojaRoupaApplication.java
@@ -0,0 +1,13 @@
+package app.GestaoLojaRoupa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoLojaRoupaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoLojaRoupaApplication.class, args);
+	}
+
+}
---
+++ GestaoLojaRoupa/src/test/java/app/GestaoLojaRoupa/GestaoLojaRoupaApplicationTests.java
@@ -0,0 +1,13 @@
+package app.GestaoLojaRoupa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoLojaRoupaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

1d91c6c880703122d659c20afa794223cb019668
ThiagoZanardi05 <thiagozanardi05@gmail.com>
2024-08-21 19:04:46 -0300

    Falta o Controller de Cliente

---
+++ GestaoLojaRoupa/src/main/java/app/GestaoLojaRoupa/Controller/ClienteController.java
@@ -0,0 +1,11 @@
+package app.GestaoLojaRoupa.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class ClienteController {
+
+
+}

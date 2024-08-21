26b05b425d56c9c42382222dbb09c48394af161d
ThiagoZanardi05 <thiagozanardi05@gmail.com>
2024-08-21 19:49:13 -0300

    Cliente finalizado

--- GestaoLojaRoupa/src/main/java/app/GestaoLojaRoupa/Controller/ClienteController.java
+++ GestaoLojaRoupa/src/main/java/app/GestaoLojaRoupa/Controller/ClienteController.java
@@ -1,11 +1,78 @@
 package app.GestaoLojaRoupa.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import app.GestaoLojaRoupa.Entity.Cliente;
+import app.GestaoLojaRoupa.Service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/cliente")
 public class ClienteController {
 
+    @Autowired
+    private ClienteService clienteService;
 
+    @PostMapping("/save")
+    public ResponseEntity<String> save(@RequestBody Cliente cliente) {
+        try {
+            String mensagem = this.clienteService.save(cliente);
+            return new ResponseEntity<>(mensagem, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> update(@RequestBody Cliente cliente, @PathVariable long id) {
+        try {
+            String mensagem = this.clienteService.update(cliente, id);
+            return new ResponseEntity<>(mensagem, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> delete(@PathVariable long id) {
+        try {
+            String mensagem = this.clienteService.delete(id);
+            return new ResponseEntity<>(mensagem, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/findAll")
+    public ResponseEntity<List<Cliente>> findAll() {
+        try {
+            List<Cliente> lista = this.clienteService.findAll();
+            return new ResponseEntity<>(lista, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/findById/{id}")
+    public ResponseEntity<Cliente> findById(@PathVariable long id) {
+        try {
+            Cliente funcionario = this.clienteService.findById(id);
+            return new ResponseEntity<>(funcionario, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/findByNome")
+    public ResponseEntity<List<Cliente>> findByNome(@RequestParam String nome) {
+        try {
+            List<Cliente> lista = this.clienteService.findByNome(nome);
+            return new ResponseEntity<>(lista, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+    }
 }

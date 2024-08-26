1ace7fa70fbc98ea17d0deb605598c5b191a0c37
knevctt <knevctt@outlook.com>
2024-08-26 18:46:20 -0300

    ta ai

--- GestaoLojaRoupa/src/main/java/app/GestaoLojaRoupa/Repository/ProdutoRepository.java
+++ GestaoLojaRoupa/src/main/java/app/GestaoLojaRoupa/Repository/ProdutoRepository.java
@@ -2,9 +2,12 @@ package app.GestaoLojaRoupa.Repository;
 
 import app.GestaoLojaRoupa.Entity.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ProdutoRepository extends JpaRepository<Produto,Long> {
     public List<Produto> findByNome(String nome);
+
+
 }
